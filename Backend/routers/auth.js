8c383a54d59270be414f4d89953b44388aff698e
DJ <social.deej@gmail.com>
2020-09-03 01:28:52 +0530

    Pushing the Latest Code Base

--- Backend/routers/auth.js
+++ Backend/routers/auth.js
@@ -16,7 +16,8 @@ router.get(
 // Callback route for google to redirect
 router.get("/auth/google/redirect", passport.authenticate('google'),(req, res, next) => {
 	user = req.user
-	res.send(user)
+	//res.send(user)
+	res.redirect(303, "http://127.0.0.1:5500/Frontend/index.html");
 });
 
 module.exports = router;
